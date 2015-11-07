73205f20e29bb736c4e9cadfedf04fc41a846fe5
dredivaris <dredivaris@gmail.com>
2015-11-07 18:49:30 -0500

    update the thinkster module to include it's new dependencies

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,11 @@
-angular
-  .module('thinkster', []);
+(function() {
+  'use strict';
+  angular
+      .module('thinkster', [
+          'thinkster.routes',
+          'thinkster.authentication'
+      ]);
+
+  angular
+      .module('thinkster.routes', ['ngRoute']);
+})();
