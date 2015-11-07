cd98149c48604c2406e77a673d393ddaf12e4595
dredivaris <dredivaris@gmail.com>
2015-11-07 15:53:07 -0500

    add RegisterController

---
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -0,0 +1,29 @@
+/**
+* Register controller
+* @namespace thinkster.authentication.controllers
+*/
+(function (){
+  'use strict';
+
+  angular
+      .module('thinkster.authentication.controllers')
+      .controller('RegisterController', RegisterController);
+
+  /**
+  * @namespace RegisterController
+  */
+  function RegisterController($location, $scope, Authentication) {
+    var vm = this;
+
+    vm.register = register;
+
+    /**
+    * @name register
+    * @desc Register a new user
+    * @memberOf thinkster.authentication.controllers.RegisterController
+    */
+    function register() {
+      Authenication.register(vm.email, vm.password, vm.username);
+    }
+  }
+})();
