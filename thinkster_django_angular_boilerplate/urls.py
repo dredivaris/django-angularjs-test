58a9242014ce39d87a75a2c021d09e751de8f253
dredivaris <dredivaris@gmail.com>
2015-11-07 14:55:17 -0500

    add an API endpoint for AccountViewSet

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,17 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url, include
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
 from thinkster_django_angular_boilerplate.views import IndexView
 
-urlpatterns = patterns(
-    '',
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
-    url('^.*$', IndexView.as_view(), name='index'),
+
+urlpatterns = patterns(
+  '',
+
+  url(r'^api/v1/', include(router.urls)),
+
+  url('^.*$', IndexView.as_view(), name='index'),
 )
+
