ea1ca7136dbc5218e28de9d0145de003f8f36aef
dredivaris <dredivaris@gmail.com>
2015-11-08 00:33:30 -0500

    add getAuthenticatedAccount, isAuthenticated, setAuthenticatedAccount, and unauthenticate methods to Authentication service

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -21,8 +21,12 @@
      * @desc The Factory to be returned
      */
     var Authentication = {
+      getAuthenticatedAccount: getAuthenticatedAccount,
+      isAuthenticated: isAuthenticated,
       login: login,
-      register: register
+      register: register,
+      setAuthenticatedAccount: setAuthenticatedAccount,
+      unauthenticate: unauthenticate
     };
     return Authentication;
 
@@ -61,7 +65,50 @@
       });
     }
 
+    /**
+     * @name getAuthenticatedAccount
+     * @desc Return the currently authenticated account
+     * @returns {object|undefined} Account if authenticated, else `undefined`
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function getAuthenticatedAccount() {
+      if (!$cookies.authenticatedAccount) {
+        return;
+      }
+
+      return JSON.parse($cookies.authenticatedAccount);
+    }
+
+    /**
+     * @name isAuthenticated
+     * @desc Check if the current user is authenticated
+     * @returns {boolean} True is user is authenticated, else false.
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function isAuthenticated() {
+      return !!$cookies.authenticatedAccount;
+    }
+
+    /**
+     * @name setAuthenticatedAccount
+     * @desc Stringify the account object and store it in a cookie
+     * @param {Object} user The account object to be stored
+     * @returns {undefined}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function setAuthenticatedAccount(account) {
+      $cookies.authenticatedAccount = JSON.stringify(account);
+    }
+
+    /**
+     * @name unauthenticate
+     * @desc Delete the cookie where the user object is stored
+     * @returns {undefined}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function unauthenticate() {
+      delete $cookies.authenticatedAccount;
+    }
+
   }
-
-
 })();
