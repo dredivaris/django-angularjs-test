34957706c04003e8aebbd3c3c8f1bb26f423c441
dredivaris <dredivaris@gmail.com>
2015-11-09 00:35:51 -0500

    redirect authenticated users to the index view in RegisterController

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -1,7 +1,7 @@
 /**
-* Register controller
-* @namespace thinkster.authentication.controllers
-*/
+ * Register controller
+ * @namespace thinkster.authentication.controllers
+ */
 (function (){
   'use strict';
 
@@ -10,18 +10,32 @@
       .controller('RegisterController', RegisterController);
 
   /**
-  * @namespace RegisterController
-  */
+   * @namespace RegisterController
+   */
   function RegisterController($location, $scope, Authentication) {
     var vm = this;
+
+    activate();
+
+    /**
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf thinkster.authentication.controllers.RegisterController
+     */
+    function activate() {
+      // If the user is authenticated, they should not be here.
+      if (Authentication.isAuthenticated()) {
+        $location.url('/');
+      }
+    }
 
     vm.register = register;
 
     /**
-    * @name register
-    * @desc Register a new user
-    * @memberOf thinkster.authentication.controllers.RegisterController
-    */
+     * @name register
+     * @desc Register a new user
+     * @memberOf thinkster.authentication.controllers.RegisterController
+     */
     function register() {
       Authentication.register(vm.email, vm.first_name, vm.last_name, vm.password, vm.username);
     }
